d8ea5e3e1f6655814621d2e6194212db99c55871
Kseniia Denisova <kdenisova@8thlight.com>
2022-03-29 18:49:51 -0400

    Add styles for Home page, remove default App files

--- ui/src/components/Home/index.js
+++ ui/src/components/Home/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import './index.css';
 
 function Home() {
   return (
-    <div>
-      <h1>Plower</h1>
+    <div className="home">
+      <div className="main-header">
+        <h1>Plower</h1>
+      </div>
     </div>
   );
 }
