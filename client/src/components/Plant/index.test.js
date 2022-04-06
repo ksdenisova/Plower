687da3ea0baf2e97cb76e0a70e4e396d9995f2ac
Kseniia Denisova <kdenisova@8thlight.com>
2022-04-06 09:58:35 -0400

    Add describe block to tests

--- client/src/components/Plant/index.test.js
+++ client/src/components/Plant/index.test.js
@@ -1,43 +1,45 @@
 import Plant from '.';
 import { render, screen } from "@testing-library/react"
 
-test('renders a plant name', () => {
-  const testPlant = { "id": "0", "name": "Test plant name" };
+describe('Plant', () => {
+  test('renders a plant name', () => {
+    const testPlant = { "id": "0", "name": "Test plant name" };
+    
+    render(<Plant plant={testPlant}/>);
   
-  render(<Plant plant={testPlant}/>);
-
-  const plantName = screen.getByText("Test plant name");
-
-  expect(plantName).toBeInTheDocument();
-});
-
-test('renders a default plant image', () => {
-  const testPlant = { "id": "0", "name": "Test plant name" };
+    const plantName = screen.getByText("Test plant name");
   
-  render(<Plant plant={testPlant}/>);
-
-  const image = screen.getByRole("img");
-
-  expect(image).toBeInTheDocument();
-  expect(image).toHaveAttribute("src", "/default_plant.png");
-});
-
-test('renders last watering date and time', () => {
-  const testPlant = { "id": "0", "name": "Test plant name", "lastWatered": "2022-03-30T12:01:00" };
+    expect(plantName).toBeInTheDocument();
+  });
   
-  render(<Plant plant={testPlant}/>);
-
-  const lastWatered = screen.getByText("Last watered: March 30, 2022 at 12:01 pm");
-
-  expect(lastWatered).toBeInTheDocument();
-});
-
-test("renders 'Not yet watered' if there are not last watering date and time", () => {
-  const testPlant = { "id": "0", "name": "Test plant name", "lastWatered": "" };
+  test('renders a default plant image', () => {
+    const testPlant = { "id": "0", "name": "Test plant name" };
+    
+    render(<Plant plant={testPlant}/>);
   
-  render(<Plant plant={testPlant}/>);
-
-  const lastWatered = screen.getByText("Not yet watered");
-
-  expect(lastWatered).toBeInTheDocument();
+    const image = screen.getByRole("img");
+  
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute("src", "/default_plant.png");
+  });
+  
+  test('renders last watering date and time', () => {
+    const testPlant = { "id": "0", "name": "Test plant name", "lastWatered": "2022-03-30T12:01:00" };
+    
+    render(<Plant plant={testPlant}/>);
+  
+    const lastWatered = screen.getByText("Last watered: March 30, 2022 at 12:01 pm");
+  
+    expect(lastWatered).toBeInTheDocument();
+  });
+  
+  test("renders 'Not yet watered' if there are not last watering date and time", () => {
+    const testPlant = { "id": "0", "name": "Test plant name", "lastWatered": "" };
+    
+    render(<Plant plant={testPlant}/>);
+  
+    const lastWatered = screen.getByText("Not yet watered");
+  
+    expect(lastWatered).toBeInTheDocument();
+  });
 });
