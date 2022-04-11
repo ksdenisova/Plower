94d945a8311fbff3097207e46df34719481f1ab3
Kseniia Denisova <kdenisova@8thlight.com>
2022-04-11 16:37:41 -0400

    Add functions for calibrating sensors

--- modules/plant_reader.js
+++ modules/plant_reader.js
@@ -2,10 +2,11 @@ const ADS1115 = require('ads1115');
 const os = require('os');
 let i2c;
 
-if (os.arch() == 'arm64')
+if (os.arch() == 'arm64') {
   i2c = require('i2c-bus');
-else
-  i2c = require('./i2c.mock.js');
+} else {
+  i2c = require('./i2c.mock');
+}
 
 const sensors = [ {"id": 1, "channel": "1+GND", "humidity": null, "dryMax": 12730, "wetMin": 5571, "available": false},
                   {"id": 2, "channel": "2+GND", "humidity": null, "dryMax": 12116, "wetMin": 5265, "available": false} ];
@@ -27,7 +28,52 @@ const readHumidity = async (channel) => {
   return humidity;
 }
 
+const calibrateDrySensors = async () => {
+  console.log("Calibrating dry sensors. Do not touch sensors");
+
+  for (let sensor of sensors) {
+    console.log("Calibrating dry sensor on channel", sensor.channel);
+    sensor.dryMax = await calibrateDrySensor(sensor.channel);
+    console.log("Save this value as dry max:", sensor.dryMax);
+  }
+}
+
+const calibrateDrySensor = async (channel) => {
+  let max = 0;
+
+  for (let i = 0; i < 10; i++) {
+    let currentMax = await readHumidity(channel);
+    max = Math.max(max, currentMax);
+  }
+
+  return max;
+}
+
+const calibrateWetSensors = async () => {
+  console.log("Calibrating wet sensors. Sensors should be placed in water");
+
+  for (let sensor of sensors) {
+    console.log("Calibrating wet sensor on channel", sensor.channel);
+    sensor.wetMin = await calibrateWetSensor(sensor.channel);
+
+    console.log("Save this value as wet min:", sensor.wetMin);
+  }
+}
+
+const calibrateWetSensor = async (channel) => {
+  let min = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < 10; i++) {
+    let currentMin = await readHumidity(channel);
+    min = Math.min(min, currentMin);
+  }
+
+  return min;
+}
+
 module.exports = {
   updateHumidity,
-  readHumidity
+  readHumidity,
+  calibrateDrySensors,
+  calibrateWetSensors
 }
--- server.js
+++ server.js
@@ -3,30 +3,39 @@ const app = express();
 const port = process.env.PORT || 3000;
 const PlantService = require('./modules/plant_service');
 const PlantReader = require('./modules/plant_reader');
-
 const schedule = require('node-schedule');
 
-const job = schedule.scheduleJob('* * * * * *', async () => {
-  await PlantReader.updateHumidity();
-});
-
-app.use(express.json());
-app.use(express.static('client/build'));
-
-app.get('/', (req, res) => {
-  res.sendFile('client/build/index.html');
-});
-
-app.get('/plants', async (req, res) => {
-  const plants = await PlantService.getPlants();
-  res.send(plants);
-});
-
-app.post('/plants', async (req, res) => {
-  await PlantService.createPlant(req.body);
-  res.send(req.body);
-})
-
-app.listen(port, () => {
-  console.log(`Plower app listening on port ${port}`)
-});
+if (process.argv[2] == "calibrate") {
+  if (process.argv[3] == "dry")
+    PlantReader.calibrateDrySensors();
+  else if (process.argv[3] == "wet")
+    PlantReader.calibrateWetSensors();
+  else {
+    console.log("Unknown command. Use 'dry' or 'wet'")
+  }
+} else {
+  const job = schedule.scheduleJob('* * * * * *', async () => {
+    await PlantReader.updateHumidity();
+  });
+  
+  app.use(express.json());
+  app.use(express.static('client/build'));
+  
+  app.get('/', (req, res) => {
+    res.sendFile('client/build/index.html');
+  });
+  
+  app.get('/plants', async (req, res) => {
+    const plants = await PlantService.getPlants();
+    res.send(plants);
+  });
+  
+  app.post('/plants', async (req, res) => {
+    await PlantService.createPlant(req.body);
+    res.send(req.body);
+  })
+  
+  app.listen(port, () => {
+    console.log(`Plower app listening on port ${port}`)
+  });
+}
